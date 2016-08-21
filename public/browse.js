0d1e0e74151079e3727c9ffba1ce22ebc5a0156b
Jakub Pavlík <severus@post.cz>
2016-08-21 22:35:26 +0200

    initialize by current date; better navigation

--- public/browse.js
+++ public/browse.js
@@ -1,32 +1,51 @@
 angular.module('calendarApp', [])
   .controller('MonthController', function($scope, $http) {
     // unchanging
+    $scope.baseUri = '/api/v0/en'
     $scope.months = [];
     for (var i = 1; i <= 12; i++) {
       $scope.months.push(i);
     }
 
     // changing
+    var today = new Date();
     $scope.calendars = null;
     $scope.selectedCalendar = 'default';
-    $scope.year = 2016;
-    $scope.month = 8;
+    $scope.year = today.getFullYear();
+    $scope.month = today.getMonth() + 1;
     $scope.days = [];
 
     $http
-      .get('/api/v0/en/calendars')
+      .get($scope.baseUri + '/calendars')
       .success(function (data) {
         $scope.calendars = data;
         $scope.selectedCalendar = data[0];
       });
 
-    this.loadMonth = function () {
+    var loadMonth = function () {
       $http
-        .get('/api/v0/en/calendars/' + $scope.selectedCalendar + '/' + $scope.year + '/' + $scope.month)
+        .get($scope.baseUri + '/calendars/' + $scope.selectedCalendar + '/' + $scope.year + '/' + $scope.month)
         .success(function (data) {
           $scope.days = data;
         });
     };
 
-    $scope.$watch('[selectedCalendar, year, month]', this.loadMonth);
+    $scope.$watch('[selectedCalendar, year, month]', loadMonth);
+
+    $scope.previousMonth = function () {
+      if ($scope.month > 1) {
+        $scope.month--;
+      } else {
+        $scope.year--;
+        $scope.month = 12;
+      }
+    };
+    $scope.nextMonth = function () {
+      if ($scope.month < 12) {
+        $scope.month++;
+      } else {
+        $scope.year++;
+        $scope.month = 1;
+      }
+    };
   });

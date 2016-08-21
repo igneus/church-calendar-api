6b9d9c1a0498cebde9a9d129a272a7eb502bc934
Jakub Pavlík <severus@post.cz>
2016-08-21 15:51:43 +0200

    allow browsing all calendars, years, months
    interface very similar to that of the original "traditional" web app

--- public/browse.js
+++ public/browse.js
@@ -1,11 +1,32 @@
 angular.module('calendarApp', [])
   .controller('MonthController', function($scope, $http) {
-    $scope.calendars = null;
-    $scope.selectedCalendar = null;
+    // unchanging
+    $scope.months = [];
+    for (var i = 1; i <= 12; i++) {
+      $scope.months.push(i);
+    }
 
-    $http.get('/api/v0/en/calendars')
+    // changing
+    $scope.calendars = null;
+    $scope.selectedCalendar = 'default';
+    $scope.year = 2016;
+    $scope.month = 8;
+    $scope.days = [];
+
+    $http
+      .get('/api/v0/en/calendars')
       .success(function (data) {
         $scope.calendars = data;
         $scope.selectedCalendar = data[0];
       });
+
+    this.loadMonth = function () {
+      $http
+        .get('/api/v0/en/calendars/' + $scope.selectedCalendar + '/' + $scope.year + '/' + $scope.month)
+        .success(function (data) {
+          $scope.days = data;
+        });
+    };
+
+    $scope.$watch('[selectedCalendar, year, month]', this.loadMonth);
   });

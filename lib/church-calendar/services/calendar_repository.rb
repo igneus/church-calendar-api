39a1951dfd4105ac2128d927971072ddf6e953b9
Jakub Pavlík <severus@post.cz>
2017-08-06 00:16:39 +0200

    delete unnecessary code

--- lib/church-calendar/services/calendar_repository.rb
+++ lib/church-calendar/services/calendar_repository.rb
@@ -13,50 +13,25 @@ module ChurchCalendar
       new YAML.load_file(yaml_config_path), data_path
     end
 
+    def_delegators :@calendars, :keys, :has_key?
+
     def [](key)
-      unless has_key? key
+      unless @calendars.has_key? key
         raise KeyError.new(key)
       end
 
-      CalendarFacade.new get_calendar_factory(key), @calendars[key]
-    end
-
-    # returns a CalendarFactory creating Calendars
-    # with a specified Sanctorale
-    def get_calendar_factory(key)
-      sanctorale = CalendariumRomanum::SanctoraleFactory
-                   .load_layered_from_files *data_files(key)
-      CalendarFactory.new sanctorale
-    end
-
-    # list of available calendars
-    def keys
-      @calendars.keys
-    end
-
-    def has_key?(key)
-      @calendars.has_key? key
-    end
-
-    # Array of data files for the given calendar;
-    # should be loaded over each other in the given order
-    def data_files(key)
-      calendar_setup = @calendars[key]
-      raise UnknownCalendarError.new(key) if calendar_setup.nil?
-
-      calendar_setup['files'].collect do |f|
+      data_files = @calendars[key]['files'].collect do |f|
         File.join @path, f
       end
+      sanctorale = CalendariumRomanum::SanctoraleFactory
+                   .load_layered_from_files *data_files
+      factory = CalendarFactory.new sanctorale
+
+      CalendarFacade.new factory, @calendars[key]
     end
 
     def metadata
       @calendars
-    end
-  end
-
-  class UnknownCalendarError < RuntimeError
-    def initialize(key)
-      super("Unknown calendar #{key.inspect}")
     end
   end
 end
